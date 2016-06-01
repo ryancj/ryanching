21106b031cfbe7934269d3579eaa2c631a66e4dd
Ryan Kayu Ching <ryanching@hotmail.com>
2016-05-31 18:12:59 -0700

    Implemented footer

--- javascript/main.js
+++ javascript/main.js
@@ -18,7 +18,7 @@ $(function() {
   //Initialize ScrollReveal
   window.sr = ScrollReveal({ reset: true });
 
-  sr.reveal('.tech-logo', { duration: 1100, distance: '20px', viewFactor: 0.5 }, 40); 
+  sr.reveal('.tech-logo', { duration: 1100, distance: '20px', viewFactor: 0.5 }, 40);
   sr.reveal('.stag-list', { duration: 1000, distance:'20px', scale: 1 }, 100);
   sr.reveal('.photo-fade', { duration: 800, distance:'20px', scale: 1 }, 40);
   sr.reveal('.fade', { duration: 700, distance:'20px', scale: 1 });
